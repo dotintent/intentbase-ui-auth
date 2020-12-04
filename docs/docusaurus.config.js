0ed36758a1e330651e9c041f1c9e89ce826a1640
jakub.jozwiak <kubencki@gmail.com>
2020-12-04 19:34:12 +0100

    feat: setup docusaurus docs

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,128 @@
+module.exports = {
+  title: 'Intentbase Docs',
+  tagline: 'Set of packages for intentbase project.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'dotintent', // Usually your GitHub org/user name.
+  projectName: 'intentbase-ui-auth', // Usually your repo name.
+  themeConfig: {
+    // googleAnalytics: {
+    // 	trackingID: '',
+    // 	anonymizeIP: true,
+    // },
+    // algolia : {
+    // 	// This is a read-only, search-only key served directly by the front-end, managed by Algolia via their
+    // 	// free DocSearch program. The key is not sensitive. See https://docsearch.algolia.com/ for more details.
+    // 	apiKey: '',
+    // 	indexName: '',
+    // },
+    navbar: {
+      title: 'Intentbase',
+      logo: {
+        alt: 'Intentbase Logo',
+        src: 'img/mobile-logo.png',
+      },
+      items: [
+        {
+          to: 'docs/ui-auth',
+          label: 'Auth Components',
+          position: 'left',
+        },
+        {
+          to: 'docs/frontend',
+          label: 'Frontend Starter',
+          position: 'left',
+        },
+        {
+          to: 'docs/backend',
+          label: 'Backend Starter',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/dotintent/intentbase-ui-auth',
+          label: 'Github',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Intentbase Packages',
+          items: [
+            {
+              label: 'UI Auth Components',
+              to: 'docs/ui-auth',
+            },
+            {
+              label: 'Frontend Starter',
+              to: 'docs/frontend',
+            },
+            {
+              label: 'Backend Starter',
+              to: 'docs/backend',
+            },
+          ],
+        },
+        {
+          title: 'Social Media',
+          items: [
+            {
+              label: 'Linkedin',
+              href: 'https://www.linkedin.com/company/dotintent/',
+            },
+            {
+              label: 'Facebook',
+              href: 'https://www.facebook.com/dotintent/',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://www.instagram.com/dotintent',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/dotintent',
+            },
+          ],
+        },
+        {
+          title: 'More about Intent',
+          items: [
+            {
+              label: 'Website',
+              href: 'https://withintent.com',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/dotintent',
+            },
+            {
+              label: 'Blog',
+              href: 'https://withintent.com/blog',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Intent. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/dotintent/intentbase-ui-auth/docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ docs/sidebars.js
@@ -0,0 +1,6 @@
+module.exports = {
+  someSidebar: {
+    'Getting Started': ['ui-auth/getting-started'],
+    'Docs Guide': ['ui-auth/style-guide', 'ui-auth/mdxTest'],
+  },
+};
