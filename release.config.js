8be6bf12a51d2e98696cbc308420d274a74dffec
jakub.jozwiak <kubencki@gmail.com>
2021-03-29 14:55:18 +0200

    feat: add semantic-release, adjust yarn setup, sort package.json

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: [
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+};
